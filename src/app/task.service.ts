ab9f894c214d7a61a83d3cb0f4dc1ae692ae9250
ahmarcela <srita.godinez.org@gmail.com>
2018-07-23 13:06:45 -0500

    CRUD

--- src/app/task.service.ts
+++ src/app/task.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+
+import { Task } from './task';
+
+const HTTP_OPTIONS = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class TaskService {
 
-  constructor() { }
+  apiUrl = 'http://localhost:3000';
+  taskUrl = `${this.apiUrl}/tasks`;
+
+  constructor(private http: HttpClient) { }
+
+  getTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(this.taskUrl)
+    .pipe(map(data => data));
+  }
 }
--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Task } from '../task';
+import { TaskService } from '../task.service';
+
 @Component({
   selector: 'app-task',
   templateUrl: './task.component.html',
   styleUrls: ['./task.component.css']
 })
 export class TaskComponent implements OnInit {
+  tasks: Task[];
 
-  constructor() { }
+  constructor(private taskService: TaskService) { }
 
   ngOnInit() {
+    this.getTasks();
   }
 
+  getTasks(): void {
+    this.taskService.getTasks()
+    .subscribe(tasks => this.tasks =tasks);
+  }
 }
