7a53f11beff6a59ae13eb3de9d7a6abcc235782b
ah-debuggirl <srita.godinez.org@gmail.com>
2018-07-24 19:47:41 -0500

    Se agrega nav y routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { TaskDetailComponent } from './task-detail/task-detail.component';
 import { UserService } from './user.service';
 import { TaskService } from './task.service';
 import { AppRoutingModule } from './/app-routing.module';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { AppRoutingModule } from './/app-routing.module';
     UserComponent,
     UserDetailComponent,
     TaskComponent,
-    TaskDetailComponent
+    TaskDetailComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
